d015e2eb4cd8d994d4a0327bf15143318913a1cb
Isna Rahmatul Khoir <isna.rahmatul@mekari.com>
2025-01-11 06:16:15 +0700

    Initial Commit

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,22 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1736550626',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
